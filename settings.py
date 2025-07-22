9c9eb54ac1d1abc229662cb66b39df5f927835dd
Avn <avanthykkha@example.com>
2025-07-22 20:23:06 +0530

    Updated settings.py

--- settings.py
+++ settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
-        'STATICFILES_DIRS' = [BASE_DIR / "core" / "static"]
+        'STATICFILES_DIRS' = [BASE_DIR / "core" / "static"],
 
         'OPTIONS': {
             'context_processors': [
